f061cffbc351d8c24d27e271f4e53be9420a232c
Ritikdata12 <ritikdata123@gmail.com>
2024-12-02 17:38:05 +0530

    latest commit

--- frontend/src/components/Tables.jsx
+++ frontend/src/components/Tables.jsx
@@ -13,7 +13,7 @@ const Tables = () => {
   const fetchData = async () => {
     setLoading(true); 
     try {
-      const response = await axios.get('https://certificate-generation-verification-83ig.vercel.app/api/admin/certificates');
+      const response = await axios.get('https://certificate-generation-verification.onrender.com/api/admin/certificates');
       setTable(response.data); 
     } catch (error) {
       setError('Error fetching data'); 
@@ -28,7 +28,7 @@ const Tables = () => {
 
   const uploadData = async (newData) => {
     try {
-      await axios.post('https://certificate-generation-verification-83ig.vercel.app/api/admin/upload', newData);
+      await axios.post('https://certificate-generation-verification.onrender.com/api/admin/upload', newData);
       fetchData(); 
     } catch (error) {
       console.error("Error uploading data", error); 
